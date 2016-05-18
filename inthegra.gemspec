5470650026da287647544acd3179b7e236127ce8
Rafael Guimarães <rafaelguimaraesweb@gmail.com>
2016-05-18 14:45:35 -0300

    Adding webmock gem and configuring spec_helper to use it

--- inthegra.gemspec
+++ inthegra.gemspec
@@ -22,6 +22,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.12"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "simplecov", "~> 0.11.2"
+  spec.add_development_dependency "webmock", "~> 2.0.2"
   spec.add_development_dependency "faraday", "~> 0.9.2"
   spec.add_development_dependency "faraday_middleware", "~> 0.10.0"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,38 @@
+require 'simplecov'
+SimpleCov.start do
+  add_group 'Inthegra', 'lib/inthegra'
+  add_group 'Specs', 'spec'
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'inthegra'
+
+require 'rspec'
+require 'webmock/rspec'
+
+RSpec.configure do |config|
+  config.include WebMock::API
+end
+
+def a_get(path)
+  a_request(:get, Inthegra.endpoint + path)
+end
+
+def a_post(path)
+  a_request(:post, Inthegra.endpoint + path)
+end
+
+def stub_get(path)
+  stub_request(:get, Inthegra.endpoint + path)
+end
+
+def stub_post(path)
+  stub_request(:post, Inthegra.endpoint + path)
+end
+
+def fixture_path
+  File.expand_path("../fixtures", __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
